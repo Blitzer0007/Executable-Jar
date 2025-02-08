f8dba4532e1197ff17a47c29bd51af09febe87d0
Blitzer0007 <104625510+Blitzer0007@users.noreply.github.com>
2025-02-08 14:11:07 +0530

    Create RunJar2.bat

---
+++ RunJar2.bat
@@ -0,0 +1,5 @@
+java -Denvironment=RT -DmethodName=Test2 -jar "%WORKSPACE%\target\ContinousIntegration-1.0-SNAPSHOT-test-jar-with-dependencies.jar"
+IF ERRORLEVEL 1 (
+  ECHO "ERROR RUNNING JAR FILE"
+  EXIT /BA 1
+)
